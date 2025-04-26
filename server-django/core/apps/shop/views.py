5ee3e20bf24f8bbfdda5a5f01d769ae0de961709
Juan Huamani <jhuamaniv@unsa.edu.pe>
2025-04-26 18:53:55 -0500

    style(shop/views): improve code readability and formatting
    Refactor code to enhance readability by removing unnecessary whitespace, improving line breaks, and ensuring consistent indentation.

--- server-django/core/apps/shop/views.py
+++ server-django/core/apps/shop/views.py
@@ -10,6 +10,7 @@ from .models import Product, Cart, CartItem, Category
 from .serializers import CartItemSerializer, DetailedProductSerializer, ProductSerializer, CartSerializer, CategorySerializer
 from .services.cart_service import CartService
 
+
 class ProductView(APIView):
     permission_classes = [IsAuthenticated]
 
@@ -19,14 +20,14 @@ class ProductView(APIView):
                 product = Product.objects.get(slug=slug)
                 serializer = DetailedProductSerializer(product)
                 return Response({'message': 'Product details retrieved successfully', 'product': serializer.data}, status=status.HTTP_200_OK)
-            
+
             products = Product.objects.all()
             serializer = ProductSerializer(products, many=True)
             return Response({'message': 'Products list retrieved successfully', 'products': serializer.data}, status=status.HTTP_200_OK)
 
         except Product.DoesNotExist:
             return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
-        
+
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -45,7 +46,7 @@ class ProductView(APIView):
                 serializer.save()
                 return Response({'message': 'Product updated successfully', 'product': serializer.data}, status=status.HTTP_200_OK)
             return Response({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-        
+
         except Product.DoesNotExist:
             return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
 
@@ -69,9 +70,10 @@ class CategoryListView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        categories = Category.objects.annotate(count=Count('product')).order_by('-count')[:5]
+        categories = Category.objects.annotate(
+            count=Count('product')).order_by('-count')[:5]
         serializer = CategorySerializer(categories, many=True)
-        
+
         return Response({'message': 'Categories retrieved successfully', 'categories': serializer.data}, status=status.HTTP_200_OK)
 
 
@@ -85,7 +87,7 @@ class CategoryDetailView(APIView):
             serializer = ProductSerializer(products, many=True)
 
             return Response({'message': 'Category products retrieved successfully', 'products': serializer.data}, status=status.HTTP_200_OK)
-        
+
         return Response({'message': 'Category not found', 'error': 'Invalid category slug'}, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -145,19 +147,20 @@ class CartItemView(APIView):
             'cart_item': CartItemSerializer(cart_item).data,
             'message': 'Item added to cart successfully'
         }, status=status.HTTP_201_CREATED)
-        
+
     def put(self, request):
         user = request.user
         cart_item_id = request.data.get('cart_item_id')
         quantity = request.data.get('quantity')
 
         cart = CartService.get_cart(user)
-        CartService.update_cart_item_quantity(cart, cart_item_id, int(quantity))
+        CartService.update_cart_item_quantity(
+            cart, cart_item_id, int(quantity))
 
         return Response({
-           'cart': CartSerializer(cart).data,
-           'message': 'Item quantity updated successfully' 
-        } , status=status.HTTP_200_OK)
+            'cart': CartSerializer(cart).data,
+            'message': 'Item quantity updated successfully'
+        }, status=status.HTTP_200_OK)
 
     def delete(self, request, item_id):
         user = request.user
@@ -167,11 +170,10 @@ class CartItemView(APIView):
         return Response({
             'message': 'Item removed from cart successfully',
             'cart': CartSerializer(cart).data
-            }, status=status.HTTP_200_OK)
+        }, status=status.HTTP_200_OK)
 
 
-class ClearCartView(APIView):           
+class ClearCartView(APIView):
     def delete(self, request):
         CartService.clear_cart(request.user)
         return Response({'message': 'Cart cleared successfully.'}, status=status.HTTP_204_NO_CONTENT)
-        
